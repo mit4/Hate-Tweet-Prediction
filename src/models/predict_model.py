955db4fb6d6292849c7be1b657f2a0886fe4b388
mit4 <88436281+mit4@users.noreply.github.com>
2023-09-10 21:10:05 +0530

    update

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import sklearn as sk
 
+train_data = pd.read_csv("../../data/processed/train_data.csv")
+test_data = pd.read_csv("../../data/processed/test_data.csv")
+
 # Predicting label for the test data
 test_data["label"] = lr.predict(X_test)
 test_data.to_csv("test_results.csv", index=False)
